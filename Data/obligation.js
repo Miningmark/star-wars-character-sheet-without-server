6d6a1d374be465c1993ed58af270b2ebb3197f2b
Markus <markus.sibbe@t-online.de>
2024-02-23 00:14:27 +0100

    add dynamic html generation for obligations

---
+++ Data/obligation.js
@@ -0,0 +1,250 @@
+
+
+export const obligations = [
+    {
+        key: "ADD",
+        name: "Addiction",
+        description: "Addiction Please see page 39 of the Edge of the Empire Core Rulebook for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook"
+    },
+    {
+        key: "BET",
+        name: "Betrayal",
+        description: "Betrayal Please see page 39 of the Edge of the Empire Core Rulebook, or page 17 of the Fly Casual Sourcebook, for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook or Page 17 Fly Casual"
+    },
+    {
+        key: "BLA",
+        name: "Blackmail",
+        description: "Blackmail Please see page 39 of the Edge of the Empire Core Rulebook for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook"
+    },
+    {
+        key: "BOU",
+        name: "Bounty",
+        description: "Bounty Please see page 39 of the Edge of the Empire Core Rulebook, page 17 of the Far Horizons Sourcebook, or page 17 of the Fly Casual Sourcebook, for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook or Page 17 Far Horizons or Page 17 Fly Casual"
+    },
+    {
+        key: "CRI",
+        name: "Criminal",
+        description: "Criminal Please see page 39 of the Edge of the Empire Core Rulebook, or page 17 of the Fly Casual Sourcebook, for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook or Page 17 Fly Casual"
+    },
+    {
+        key: "DEBT",
+        name: "Debt",
+        description: "Debt Please see page 39 of the Edge of the Empire Core Rulebook, page 17 of the Far Horizons Sourcebook, or page 17 of the Fly Casual Sourcebook, for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook or Page 17 Far Horizons or Page 17 Fly Casual"
+    },
+    {
+        key: "DUT",
+        name: "Dutybound",
+        description: "Dutybound: Please see page 39 of the Edge of the Empire Core Rulebook for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook"
+    },
+    {
+        key: "FAM",
+        name: "Family",
+        description: "Family Please see page 39 of the Edge of the Empire Core Rulebook, page 17 of the Far Horizons Sourcebook, or page 17 of the Fly Casual Sourcebook, for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook or Page 17 Far Horizons or Page 17 Fly Casual"
+    },
+    {
+        key: "FAV",
+        name: "Favor",
+        description: "Favor Please see page 39 of the Edge of the Empire Core Rulebook, page 17 of the Far Horizons Sourcebook, or page 17 of the Fly Casual Sourcebook, for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook or Page 17 Far Horizons or Page 17 Fly Casual"
+    },
+    {
+        key: "OATH",
+        name: "Oath",
+        description: "Oath Please see page 39 of the Edge of the Empire Core Rulebook for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook"
+    },
+    {
+        key: "OBS",
+        name: "Obsession",
+        description: "Obsession Please see page 39 of the Edge of the Empire Core Rulebook for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook"
+    },
+    {
+        key: "RESP",
+        name: "Responsibility",
+        description: "Responsibility Please see page 39 of the Edge of the Empire Core Rulebook, or page 17 of the Far Horizons Sourcebook, for details.",
+        source: "Page 39 Edge of the Empire Core Rulebook or Page 17 Far Horizons"
+    },
+    {
+        key: "SPON",
+        name: "Sponsorship",
+        description: "Sponsorship Please see page 19 of Enter the Unknown Sourcebook for details.",
+        source: "Page 19 Enter the Unknown"
+    },
+    {
+        key: "FERV",
+        name: "Fervor",
+        description: "Fervor Please see page 19 of Enter the Unknown Sourcebook for details.",
+        source: "Page 19 Enter the Unknown"
+    },
+    {
+        key: "ADRENALINE",
+        name: "Adrenaline Rush",
+        description: "Adrenaline Rush Please see page 17 of Dangerous Covenants Sourcebook for details.",
+        source: "Page 17 Dangerous Covenants"
+    },
+    {
+        key: "COLLATERAL",
+        name: "Collateral Accountability",
+        description: "Collateral Accountability Please see page 17 of Dangerous Covenants Sourcebook for details.",
+        source: "Page 17 Dangerous Covenants"
+    },
+    {
+        key: "SCORE",
+        name: "A Score to Settle",
+        description: "A Score to Settle Please see page 17 of Dangerous Covenants Sourcebook for details.",
+        source: "Page 17 Dangerous Covenants"
+    },
+    {
+        key: "PRICENAME",
+        name: "The Price of a Name",
+        description: "The Price of a Name Please see page 17 of Dangerous Covenants Sourcebook for details.",
+        source: "Page 17 Dangerous Covenants"
+    },
+    {
+        key: "DISGRACED",
+        name: "Disgraced",
+        description: "Disgraced Please see page 17 of the Far Horizons Sourcebook for details.",
+        source: "Page 17 Far Horizons"
+    },
+    {
+        key: "PHILAND",
+        name: "Philanderer",
+        description: "Philanderer Please see page 17 of the Far Horizons Sourcebook for details.",
+        source: "Page 17 Far Horizons"
+    },
+    {
+        key: "EXILED",
+        name: "Exiled",
+        description: "Exiled Please see page 17 of the Far Horizons Sourcebook for details.",
+        source: "Page 17 Far Horizons"
+    },
+    {
+        key: "CONTRACTED",
+        name: "Contracted",
+        description: "Contracted Please see page 17 of the Far Horizons Sourcebook for details.",
+        source: "Page 17 Far Horizons"
+    },
+    {
+        key: "WITNESS",
+        name: "Witness Protection",
+        description: "Witness Protection Please see page 17 of the Far Horizons Sourcebook for details.",
+        source: "Page 17 Far Horizons"
+    },
+    {
+        key: "PACIFIST",
+        name: "Pacifist",
+        description: "Pacifist Please see page 17 of the Far Horizons Sourcebook for details.",
+        source: "Page 17 Far Horizons"
+    },
+    {
+        key: "FRONTIER",
+        name: "Frontier Justice",
+        description: "Frontier Justice Please see page 17 of the Far Horizons Sourcebook for details.",
+        source: "Page 17 Far Horizons"
+    },
+    {
+        key: "BASE",
+        name: "Base of Operation",
+        description: "Base of Operation Please see page 82 of the Far Horizons Sourcebook for details.",
+        source: "Page 82 Far Horizons"
+    },
+    {
+        key: "ANTAG",
+        name: "Antagonist",
+        description: "Antagonist Please see page 17 of the Fly Casual Sourcebook for details.",
+        source: "Page 17 Fly Casual"
+    },
+    {
+        key: "BADREP",
+        name: "Bad Reputation",
+        description: "Bad Reputation Please see page 17 of the Fly Casual Sourcebook for details.",
+        source: "Page 17 Fly Casual"
+    },
+    {
+        key: "NOTORIETY",
+        name: "Notoriety",
+        description: "Notoriety Please see page 17 of the Fly Casual Sourcebook for details.",
+        source: "Page 17 Fly Casual"
+    },
+    {
+        key: "HIGHRISK",
+        name: "High Risk",
+        description: "High Risk Please see page 17 of the Fly Casual Sourcebook for details.",
+        source: "Page 17 Fly Casual"
+    },
+    {
+        key: "SERVITUDE",
+        name: "Servitude",
+        description: "Servitude Please see page 17 of the Fly Casual Sourcebook for details.",
+        source: "Page 17 Fly Casual"
+    },
+    {
+        key: "UNDERCONTRACT",
+        name: "Under Contract",
+        description: "Under Contract Please see page 17 of the Fly Casual Sourcebook for details.",
+        source: "Page 17 Fly Casual"
+    },
+    {
+        key: "CREW",
+        name: "Crew",
+        description: "Crew Please see page 18 of the Special Modifications Sourcebook for details.",
+        source: "Page 18 Special Modifications"
+    },
+    {
+        key: "CONTRACT",
+        name: "Contract",
+        description: "Contract Please see page 18 of the Special Modifications Sourcebook, or page 19 of the No Disintegrations Sourcebook, for details.",
+        source: "Page 18 Special Modifications or Page 19 No Disintegrations"
+    },
+    {
+        key: "FAILEDREP",
+        name: "Failed Installation/Repair",
+        description: "Failed Installation/Repair Please see page 18 of the Special Modifications Sourcebook for details.",
+        source: "Page 18 Special Modifications"
+    },
+    {
+        key: "UNFINBUS",
+        name: "Unfinished Business",
+        description: "Unfinished Business Please see page 18 of the Special Modifications Sourcebook for details.",
+        source: "Page 18 Special Modifications"
+    },
+    {
+        key: "THRILLSEEK",
+        name: "Thrill Seeker",
+        description: "Thrill Seeker Please see page 19 of the No Disintegrations Sourcebook for details.",
+        source: "Page 19 No Disintegrations"
+    },
+    {
+        key: "VIGILANTE",
+        name: "Vigilante",
+        description: "Vigilante Please see page 19 of the No Disintegrations Sourcebook for details.",
+        source: "Page 19 No Disintegrations"
+    },
+    {
+        key: "RULEBREAKER",
+        name: "Rule Breaker",
+        description: "Rule Breaker Please see page 19 of the No Disintegrations Sourcebook for details.",
+        source: "Page 19 No Disintegrations"
+    },
+    {
+        key: "KEEPERFAITH",
+        name: "Keeper of the Faith",
+        description: "Keeper of the Faith Please see page 19 of the No Disintegrations Sourcebook for details.",
+        source: "Page 19 No Disintegrations"
+    },
+    {
+        key: "FAME",
+        name: "Fame",
+        description: "Fame Please see page 19 of the No Disintegrations Sourcebook for details.",
+        source: "Page 19 No Disintegrations"
+    }
+];
--- js/newCharacter.js
+++ js/newCharacter.js
@@ -1,6 +1,13 @@
 import { hooks } from "../Data/hooks.js";
 import { attitudes } from "../Data/attitudes.js";
 import { determineBackgrounds } from "../Data/determineBackground.js";
+import { obligations } from "../Data/obligation.js";
+
+
+
+
+let obligationQuantity = 0;
+
 
 
 const determineBackgroundDropdown = document.getElementById("determineBackground");
@@ -46,3 +53,30 @@ adventureHookDropdown.addEventListener("input", (event) => {
     adventureHookDescription.textContent = hooks.filter((hook) => hook.key === input).map((hook) => hook.description).join();
     adventureHookSource.textContent = hooks.filter((hook) => hook.key === input).map((hook) => hook.source).join();
 });
+
+
+const obligationInputForm = document.getElementById("obligationInputForm");
+const newObligationButton = document.getElementById("newObligation");
+
+newObligationButton.addEventListener("click", (event) => {
+    obligationInputForm.innerHTML += `
+                    <fieldset>
+                        <legend>Obligation Nr.${obligationQuantity + 1}</legend>
+                        <select id="obligationType${obligationQuantity + 1}">
+                            <option value="">-SELECT OPTION-</option>
+                        </select>
+                        <p><strong>Description: </strong><span id="obligationTypeDescription${obligationQuantity + 1}"></span></p>
+                        <p><strong>Sources: </strong><span id="obligationTypeSource${obligationQuantity + 1}"></span></p>
+                        <label class="input">
+                            <input class="inputField" id="obligationValue${obligationQuantity + 1}" type="text" placeholder=" " autocomplete="off" required/>
+                            <span class="inputLabel">Obligation Value:</span>
+                        </label>
+                        <label class="input">
+                            <textarea class="inputField" id="obligationDescription${obligationQuantity + 1}" type="text" placeholder=" " autocomplete="off" required rows="5" cols="50"></textarea>
+                            <span class="inputLabel">Obligation Description:</span>
+                        </label>
+                    </fieldset> 
+                    `;
+    obligationQuantity ++;
+});
+
